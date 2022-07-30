40d0cfb928682430d5764246a8c4b933aac3f973
Angel Arshev <angel.arshev.97@gmail.com>
2022-07-30 13:52:05 +0300

    adding delete, edit, guard

--- client/src/components/book-list/book-details/BookDetails.js
+++ client/src/components/book-list/book-details/BookDetails.js
@@ -1,20 +1,39 @@
 import * as bookService from '../../../services/bookService'
-import { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../../contexts/AuthContext";
 
 
 export const BookDetails = ({
 }) => {
     const { bookId } = useParams();
     const [currentGame, setCurrentBook] = useState({});
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    console.log(user);
 
     useEffect(() => {
         bookService.getOne(bookId)
             .then(result => {
                 setCurrentBook(result);
             });
-    })
+    }, [])
 
+    const cancelClickHandler = () => {
+        navigate(`/catalog`);
+    }
+
+    const bookDeleteHandler = () => {
+        const confirmation = window.confirm('Are you sure you want to delete this book?');
+
+        if (confirmation) {
+            bookService.remove(bookId)
+                .then(() => {
+                    navigate('/catalog');
+                })
+        }
+    }
 
     return (
         <div className="bookItem">
@@ -23,9 +42,13 @@ export const BookDetails = ({
             <p>{currentGame.author}</p>
             <p>{currentGame.genre}</p>
             <p>{currentGame.details}</p>
-            <button>Edit</button>
-            <button>Delete</button>
-            
+            <Link to={`/catalog/${bookId}/edit`} className="button">
+                Edit
+            </Link>
+            <button onClick={bookDeleteHandler}>Delete</button>
+            <button id="action-cancel" className="btn" type="button" onClick={cancelClickHandler}>
+                Back to library
+            </button>
         </div>
     )
 }
